c7b02385d0358e27b3cf1a22f41daec218f2cace
Kanan <kvlrdhrs@gmail.com>
2024-03-06 00:56:42 +0200

    finished exercisesXP

--- Week1/Day4/test.py
+++ Week1/Day4/test.py
@@ -1,8 +1,21 @@
-magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
 
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich",
+                   'Chicken sandwich', "Pastrami sandwich"]
 
-def make_great():
-    global magician_names
-    magician_names = [f"{name} the Great" for name in magician_names]
+# Remove all occurrences of "Pastrami sandwich"
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
 
-make_great()
+# Create an empty list for finished sandwiches
+finished_sandwiches = []
+
+# Prepare the orders
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    finished_sandwiches.append(current_sandwich)
+    print(f"I made your {current_sandwich.lower()}")
+
+# Print a message listing each sandwich that was made
+print("\nList of sandwiches made:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
