6ff13161b362056c59324d9f63a97c8caf8028fd
Kanan <kvlrdhrs@gmail.com>
2024-03-06 00:56:24 +0200

    finished exercisesXP

--- Week1/Day2/ExercisesXP/code.py
+++ Week1/Day2/ExercisesXP/code.py
@@ -1,93 +1,64 @@
-#Exercise 1 : Set
+# Exercise 1 : Set
 
-my_fav_numbers = {23} # create a set
-my_fav_numbers.add(666) # add smthng in a set
-my_fav_numbers #we cant remove the last nunber in the set
+my_fav_numbers = {23}  # create a set
+my_fav_numbers.add(666)  # add smthng in a set
 
-friend_fav_numbers = [100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300,100,200,300] # create a list with duplicates
+# we cant remove the last number in the set
+
+friend_fav_numbers = [100, 200, 300, 100, 200, 300, 100, 200, 300, 100, 200, 300, 100, 200, 300, 100, 200, 300, 100,
+                      200, 300]  # create a list with duplicates
 friend_fav_numbers = set(friend_fav_numbers)
-friend_fav_numbers
 
 our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
-our_fav_numbers
+
+# Exercise 2: Tuple
+# nope, they are immutable
 
 
+# Exercise 3: List
 
-
-
-#Exercise 3: List
-
-#remove items
 basket = ["Banana", "Apples", "Oranges", "Blueberries"]
-
+# remove items
 list_to_remove = ('Banana', 'Blueberries')
 for item in list_to_remove:
     basket.remove(item)
 print(basket)
-#add items
+# add items
 basket.append('Kiwi')
 print(basket)
 
 basket.insert(0, 'Apples')
 print(basket)
 
-#count apples
+# count apples
 print(basket.count('Apples'))
 
-#clear basket
+# clear basket
 basket.clear()
 
-#print
+# print
 print(basket)
 
+# Exercise 4: Floats
 
-
-
-
-
-
-
-
-
-
-
-#Exercise 4: Floats
-
-#float is a type of obj. The main difference between an int and a float in the type of numbers they can represent.
-
+# float is a type of obj. The main difference between an int and a float in the type of numbers they can represent.
 num = 0.5
-list1 = [x + num for x in range(1000) if x > 0 and x <=4]
-list2 = [x for x in range(1000) if x > 1 and x <=5]
+list1 = [x + num for x in range(1000) if 0 < x <= 4]
+list2 = [x for x in range(1000) if 1 < x <= 5]
 list3 = list1 + list2
 list3.sort()
 print(list3)
 
-
-#another way to generate a sequence of floats
-
-
-
-
-
-
-
-#Exercise 5: For Loop
+# Exercise 5: For Loop
 
 for num in range(21):
     print(num)
 
-
-
 for num in range(21):
-        if num % 2 == 0:
-            print(num)
+    if num % 2 == 0:
+        print(num)
 
-
-
-
-
-
-#Exercise 6 : While Loop
+# Exercise 6 : While Loop
 my_name = 'Kanan'
 
 while True:
@@ -99,28 +70,17 @@ while True:
     else:
         print("nope, try again")
 
-
-
-
-
-#Exercise 7: Favorite Fruits
+# Exercise 7: Favorite Fruits
 
 fav_fruit = input("enter your favorite fruit(s). separated by a single space")
-
 fav_fruit_list = list(fav_fruit.split())
-
 any_fruit = input("enter a name of any fruit")
 if any_fruit in fav_fruit_list:
     print("You chose one of your favorite fruits! Enjoy!")
 else:
     print("You chose a new fruit. I hope you enjoy")
 
-
-
-
-
-
-#Exercise 8: Who Ordered A Pizza ?
+# Exercise 8: Who Ordered A Pizza ?
 
 total_price = 10  # Base price for the pizza
 toppings_list = []  # Use a list to store toppings
@@ -142,10 +102,7 @@ for topping in toppings_list:
 print(f"\nTotal price: ${total_price}")
 
 
-
-
-
-#Exercise 9: Cinemax
+# Exercise 9: Cinemax
 
 def calculate_ticket_cost(age):
     if age < 3:
@@ -154,28 +111,51 @@ def calculate_ticket_cost(age):
         return 10
     elif age > 12:
         return 15
-    
 
-#calculate the total cost of family tickets
+
+# calculate the total cost of family tickets
 num_people = int(input("enter the number of people in the family"))
 total_cost = 0
 
 for _ in range(num_people):
     person_age = int(input("enter the age of a person"))
-    total_cost +=calculate_ticket_cost(person_age)
+    total_cost += calculate_ticket_cost(person_age)
 
 print(f"total cost for the family tickets {total_cost}")
-
-
 
 teenagers = ["test1", "test2", "test3", "test4", "test4"]
 
 for teen in teenagers[:]:  # Iterate over a copy of the list to avoid modifying it while iterating
     age = int(input(f"Enter the age of {teen}: "))
-    
+
     if not (16 <= age <= 21):
         teenagers.remove(teen)
         print(f"Sorry, {teen} is not permitted to watch the movie.")
 
 print("\nFinal list of teenagers permitted to watch the movie:")
 print(teenagers)
+
+
+# Exercise 10 : Sandwich Orders
+
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich",
+                   'Chicken sandwich', "Pastrami sandwich"]
+
+# Remove all occurrences of "Pastrami sandwich"
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+# Create an empty list for finished sandwiches
+finished_sandwiches = []
+
+# Prepare the orders
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    finished_sandwiches.append(current_sandwich)
+    print(f"I made your {current_sandwich.lower()}")
+
+# Print a message listing each sandwich that was made
+print("\nList of sandwiches made:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
+
