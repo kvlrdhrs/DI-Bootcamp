15b91b6f155b8068b55ceb8f8193ccf6367d860e
Kanan <kvlrdhrs@gmail.com>
2024-03-05 15:43:10 +0200

    day3 exers

---
+++ Week1/Day3/ExercisesXP/code.py
@@ -0,0 +1,68 @@
+# Exercise 1 : Convert Lists Into Dictionaries
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dict1 = {key: value for key, value in zip(keys, values)}
+print(dict)
+
+
+# Exercise 2 : Cinemax
+def calculate_ticket_cost(age):
+    # create a function to calculate ticket cost by age
+    if age < 3:
+        return 0
+    if 3 <= age <= 12:
+        return 10
+    else:
+        return 15
+
+
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+
+# calculate ticket cost for each family member
+for keys, values in family.items():
+    print(f"the ticket price for {keys} is {calculate_ticket_cost(values)}")
+
+# calculate total tickets cost
+values_list = list(family.values())
+ticket_cost = 0
+for value in values_list:
+    ticket_cost += calculate_ticket_cost(value)
+print(f"total ticket cost for family: {ticket_cost}")
+
+
+# Exercise 3: Zara
+brand = {
+    'name': 'Zara',
+    'creation_date': '1975',
+    'creator_name': 'Amancio' 'Ortega' 'Gaona',
+    'type_of_clothes': ['men', 'women', 'children', 'home'],
+    'international_competitors': ['Gap', 'H&M', 'Benetton'],
+    'number_stores': '7000',
+    'major_color': {
+        'France': 'blue',
+        'Spain': 'red',
+        'US': 'pink' 'green',
+    }
+}
+
+# changing the number_stores
+brand['number_stores'] = '2'
+print(brand)
+
+# describing zara's clients
+for keys, values in brand.items():
+    if keys == 'type_of_clothes':
+        print(f"Zara's clients are {values}")
+
+# add new key in dictionary
+brand['country_creation'] = 'Spain'
+
+# task 6, add new store with if
+for keys, values in brand.items():
+    if keys == 'international_competitors':
+        brand['international_competitors'].append('Desigual')
+
+# delete some info using pop
+brand['creation_date'].pop()
+print(brand)
--- Week1/Day3/test.py
+++ Week1/Day3/test.py
@@ -1,6 +1,6 @@
-names = ['name1', 'name2', 'name3', 'name4']
-grades = [100, 90, 80, 70]
-
-grades_dict = {key:value+10 for key, value in zip(names, grades)}
-
-print(grades_dict)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
