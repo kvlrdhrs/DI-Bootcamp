c9c7bc3c8cdc09350281dd34d637deb82cb114f0
Kanan <kvlrdhrs@gmail.com>
2024-03-07 12:16:47 +0200

    test

---
+++ Week1/Day5/Exercises/Mini-Project-Tic Tac Toe.py
@@ -0,0 +1,20 @@
+board = [
+    ['col', 'col', 'col']
+    ['col', 'col', 'col']
+    ['col', 'col', 'col']
+]
+
+def display_board():
+    pass
+
+
+def player_input(player):
+    pass
+
+
+def check_win():
+    pass
+
+
+def play():
+    pass
