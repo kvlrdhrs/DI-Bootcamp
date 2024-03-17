8caa01e2366b4575cced07d51bda81d7aa554374
Kanan <kvlrdhrs@gmail.com>
2024-03-17 16:39:55 +0200

    finished XP Gold

---
+++ Week3/Day1/XP Gold.sql
@@ -0,0 +1,8 @@
+-- select last_name, first_name, birth_date from students
+-- order by last_name asc limit 4
+
+-- select last_name, first_name, birth_date from students
+-- order by birth_date desc limit 1
+
+-- select last_name, first_name, birth_date from students
+-- offset 2 limit 3
