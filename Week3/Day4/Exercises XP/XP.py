acad2d62d654469589f2bb102029499fda72a722
Kanan <kvlrdhrs@gmail.com>
2024-03-20 12:10:25 +0200

    created db and tbl

---
+++ Week3/Day4/Exercises XP/XP.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host = 'localhost',
+    port = '5432',
+    user = 'postgres',
+    password = '232323'
+)
+
+cur=conn.cursor()
+conn.autocommit=True
+
+cur.execute('DROP DATABASE if exists restaurant_mm;')
+cur.execute('CREATE DATABASE restaurant_mm;')
+
+cur.execute('DROP TABLE if exists menu_items')
+create_table ="""
+    CREATE TABLE menu_items(
+    item_id serial primary key,
+    item_name varchar(30) not null,
+    item_price smallint default 0
+    )
+"""
+cur.execute(create_table)
+
+cur.close()
+conn.close()
