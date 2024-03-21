36a61e555353c938ee3bdf4a844d7fcb0d34e524
Kanan <kvlrdhrs@gmail.com>
2024-03-21 11:18:10 +0200

    test1

---
+++ Week3/Day5/hackaton.py
@@ -0,0 +1,8 @@
+# FUll_name, age, city, Tel.num., email, gender,  list of need - DB volunteers seekers
+# FUll_name, age, city, Tel.num., email, gender,  info (offer, skills) - DB volunteers
+
+# we will use classes and methods to create user_menu and so on... (OOP)
+
+# # TG BOT 
+# SQL DB with data
+# PyTHON CODE using API to connect
